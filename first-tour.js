ed98dc3df1a2b6ff11b9bb083a7e8ac1d974f5ab
Arpan Tolat <Arpan Tolat>
2016-10-11 17:18:38 -0700

    First Commit

---
+++ first-tour.js
@@ -0,0 +1,18 @@
+
+//Assign Ids to elements::
+ // Define the tour!
+    var tour = {
+      id: "hello-hopscotch",
+      steps: [
+        {
+          title: "My Header",
+          content: "This is the header of my page.",
+          target: "ppm_nav_favs",
+          placement: "right"
+        }
+      ]
+    };
+
+    // Start the tour!
+    hopscotch.startTour(tour);
+        
